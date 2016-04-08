1545e94e61e4221816955d3951df5c9f6167a27d
phuntgipxy <phunt@gipxy.com>
2016-04-08 22:16:48 +0700

    Phu commit

--- src/routes/api/handlers.js
+++ src/routes/api/handlers.js
@@ -25,19 +25,6 @@ var Q_FIELD = {
 
 var internals = {};
 
-function stringToDate(_date,_format,_delimiter)
-{
-    var formatLowerCase=_format.toLowerCase();
-    var formatItems=formatLowerCase.split(_delimiter);
-    var dateItems=_date.split(_delimiter);
-    var monthIndex=formatItems.indexOf("mm");
-    var dayIndex=formatItems.indexOf("dd");
-    var yearIndex=formatItems.indexOf("yyyy");
-    var month=parseInt(dateItems[monthIndex]);
-    month-=1;
-    var formatedDate = new Date(dateItems[yearIndex],month,dateItems[dayIndex]);
-    return formatedDate;
-}
 
 function _filterResult(allAds, queryCondition) {
 
