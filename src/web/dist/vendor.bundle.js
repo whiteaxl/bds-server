4a776068e8c1881e25b624364ad079a80013f0bc
hungdqht <hungdq@gipxy.com>
2016-05-25 22:02:16 +0700

    fix 404

--- src/web/dist/vendor.bundle.js
+++ src/web/dist/vendor.bundle.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "0103ed37a2a3ddc47102"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "0230fe008e96d45d68d9"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
