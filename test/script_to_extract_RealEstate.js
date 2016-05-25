515e80de56c46b5184391533a56e052cac0e57a8
phuntgipxy <phunt@gipxy.com>
2016-05-25 22:58:43 +0700

    commit RealEstateExtractor tìm tất cả laoij bds theo ngày đăng tin ví dụ tìm theo ngày 25 tháng 5 năm 2016 thì truyền vào là 20160525 (YYYYMMDD)

---
+++ test/script_to_extract_RealEstate.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var RealEstateExtractor = require("../src/lib/RealEstateExtractor");
+
+function extractRealEstateWithNgayDangTin(sub, from, to,ngayDangTin) {
+
+var bdsEx = new RealEstateExtractor();
+
+    bdsEx.extractRealEstateWithLimit(sub,from,to,ngayDangTin);
+
+}
+
+
+extractRealEstateWithNgayDangTin("http://batdongsan.com.vn/nha-dat-ban", 1, 1,"20160525");
+
