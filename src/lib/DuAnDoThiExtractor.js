d73cd41264045577aa9ba7a24317536eba133a82
phuntgipxy <phunt@gipxy.com>
2016-06-22 22:03:59 +0700

    phu commit

---
+++ src/lib/DuAnDoThiExtractor.js
@@ -0,0 +1,103 @@
+'use strict'
+
+//extract data from bds.com
+
+var osmosis = require('osmosis');
+//html entity coding
+var entities = require("entities");
+var logUtil = require("./logUtil");
+var placeUtil = require('./placeUtil');
+var util = require("./utils");
+var DuAn = require("../dbservices/DuAn");
+var duAn = new DuAn();
+var striptags = require('striptags');
+class DuAnExtractor {
+	constructor() {
+	}
+
+	//rootURL = http://batdongsan.com.vn/cao-oc-van-phong
+	extractWithLimit(rootURL, start, end) {
+		console.log("Enter extractWithLimit .... " + start + ", " + end);
+		var startDate = new Date();
+		var count = start-1;
+		var _done = () => {
+			count++;
+		};
+
+		var i = start;
+		for (i=start; i<=end; i++) {
+			console.log("Extracting for page: " + i);
+			this.extractOnePage(rootURL + '/p'+i, _done);
+		}
+
+		var myInterval = setInterval(function(){
+			if (count==end) {
+				console.log('=================> DONE in ' + (new Date() - startDate) + 'ms');
+				clearInterval(myInterval);
+				//handleDone();
+			}
+		}, 1000);
+	}
+
+
+	extractOnePage(url, handleDone) {
+		osmosis
+			.get(url)
+			.find('.pjl-item')
+			.follow('.pjl-img a@href')
+			.set({
+				'name'			:'.prj-content .pc-text > h1',
+				'diachi'		:'.prj-content .pc-text-address[1] > p',
+				'hdLat'		:'.container-default input[id="hddLatitude"]@value',
+				'hdLong'	:'.container-default input[id="hddLongtitude"]@value',
+				'duAnID'    :'#form1 :source'
+			})
+			.data(function(duan) {
+				let adduan = {
+					ten:  duan.name,
+					tenKhongDau:  util.locDau(duan.name),
+					diaChi: util.removeAllHtmlTagAndReplaceOneString(duan.diachi,"Địa chỉ:"),
+					geo:{
+						lat: Number(duan.hdLat),
+						lon: Number(duan.hdLong)
+					},
+					duAnID : duan.duAnID
+				}
+
+
+				if(adduan.duAnID){
+					var idx = (adduan.duAnID).indexOf("form1");
+					adduan.duAnID =  (adduan.duAnID).substring(1,idx-4);
+					var idx2 = (adduan.duAnID).lastIndexOf("/");
+					adduan.duAnID =  (adduan.duAnID).substring(idx2+1);
+					adduan.duAnID =  (adduan.duAnID).trim();
+					var idx3 = (adduan.duAnID).length;
+					adduan.duAnID =  (adduan.duAnID).substring(0,idx3-1);
+					adduan.duAnID =  "DA_"+ (adduan.duAnID);
+
+				}
+				else{
+					adduan.duAnID = "DA_" + adduan.tenKhongDau;
+				}
+
+				adduan.type = "DuAn";
+				console.log("1111111");
+				console.log(adduan);
+				duAn.upsert(adduan);
+				console.log("2222222");
+			})
+
+			.log(console.log)
+			.error(console.log)
+			.debug(console.log)
+			.done(() => {
+				console.log("Done all!");
+				handleDone();
+			})
+	}
+
+
+}
+
+module.exports = DuAnExtractor;
+
---
+++ test/script_to_extract_DoThiRealEstate.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var DoThiExtractor = require("../src/lib/DoThiExtractor");
+
+function extractDoThiRealEstateWithNgayDangTin(sub, from, to,ngayDangTin) {
+
+var bdsEx = new DoThiExtractor();
+
+    bdsEx.extractWithLimit(sub,from,to);
+
+}
+
+
+extractDoThiRealEstateWithNgayDangTin("http://dothi.net/ban-nha-biet-thu-lien-ke.htm", 1, 1,"20160604");
+
