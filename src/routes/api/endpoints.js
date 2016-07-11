edb6516f50d8ba087ff8560e200c621dd1a6ee18
tinygipxy <hoai0906@gmail.com>
2016-07-11 11:08:03 +0800

    homeData4App api - allow empty title2

--- src/routes/api/endpoints.js
+++ src/routes/api/endpoints.js
@@ -579,7 +579,7 @@ internals.endpoints = [
           status: Joi.number(),
           data: Joi.array().items(Joi.object({
             title1 : Joi.string(),
-            title2 : Joi.string().allow(null),
+            title2 : Joi.string().allow(null).allow(''),
             data : Joi.array().items(Joi.object({
               adsID : Joi.string(),
               giaFmt : Joi.string(),
