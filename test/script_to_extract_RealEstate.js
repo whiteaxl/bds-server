8fc9a18fea190ae7259dd08cc0328ef12eb2be39
phuntgipxy <phunt@gipxy.com>
2016-08-21 11:54:32 +0700

    commit

--- test/script_to_extract_RealEstate.js
+++ test/script_to_extract_RealEstate.js
@@ -7,15 +7,15 @@ var bdsEx = new RealEstateExtractor();
 //var extractedDate = "20160702";
 var extractedDate = null;
 
-for (var i=5; i<=6; i++) {
-  bdsEx.extractRealEstateWithLimit("http://batdongsan.com.vn/nha-dat-cho-thue/-1/-1/-1/" + i, 30, 60, extractedDate);
-  bdsEx.extractRealEstateWithLimit("http://batdongsan.com.vn/nha-dat-ban/-1/-1/-1/" + i, 30, 60, extractedDate);
-}
+// for (var i=5; i<=6; i++) {
+//   bdsEx.extractRealEstateWithLimit("http://batdongsan.com.vn/nha-dat-cho-thue/-1/-1/-1/" + i, 30, 60, extractedDate);
+//   bdsEx.extractRealEstateWithLimit("http://batdongsan.com.vn/nha-dat-ban/-1/-1/-1/" + i, 30, 60, extractedDate);
+// }
 
 //bdsEx.extractRealEstateWithLimit("http://batdongsan.com.vn/nha-dat-cho-thue/-1/-1/-1/"+1, 1, 5,extractedDate);
 
 
 //bdsEx.extractRealEstateWithLimit("http://batdongsan.com.vn/nha-dat-ban/-1/-1/-1/"+1, 1, 5);
 
-//extractRealEstateWithNgayDangTin("http://batdongsan.com.vn/ban-can-ho-chung-cu", 1, 1,"20160702");
+bdsEx.extractRealEstateWithLimit("http://batdongsan.com.vn/ban-can-ho-chung-cu", 1, 1,"20160821");
 
