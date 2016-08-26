f43adbd1a613428a68d542c5aed4acd45cc07290
tim <tim.hung.dao@gmail.com>
2016-08-26 22:42:42 +0700

    upadte mobile detail

--- src/web/app/mobile/MobileDetailCtrl.js
+++ src/web/app/mobile/MobileDetailCtrl.js
@@ -4,10 +4,24 @@
 	angular.module('bds').controller(controllerId,function ($rootScope, $http, $scope, $state, HouseService, NewsService, NgMap, $window,$timeout,$location){
 		var vm = this;
 		vm.adsID = $state.params.adsID;
+		vm.marker = {
+			id: 1,
+			coords: {
+				latitude: 	16.0439,
+				longitude: 	108.199
+			},
+			content: undefined,
+			data: 'test'
+		}
+		vm.center= [21.0363818591319,105.80105538518103];
 		HouseService.detailAds({adsID: vm.adsID, userID: $rootScope.user.userID}).then(function(res){
 			//console.log("res.data " + res.data.ads);
 			$rootScope.user.lastViewAds = vm.adsID;
 			vm.ads = res.data.ads;
+			vm.ads.chiTietThuGon = vm.ads.chiTiet;
+			if(vm.ads.chiTiet.length > 30){
+				vm.ads.chiTietThuGon = vm.ads.chiTiet.substring(0,300);				
+			}
 			vm.ads.place.diaChinh.tinhKhongDau =  window.RewayUtil.locDau(vm.ads.place.diaChinh.tinh);
 			vm.ads.place.diaChinh.huyenKhongDau =  window.RewayUtil.locDau(vm.ads.place.diaChinh.huyen);
 			vm.placeSearchText = vm.ads.place.diaChinh.huyen + "," + vm.ads.place.diaChinh.tinh;
@@ -17,6 +31,10 @@
 			var price_max = window.RewayListValue.filter_max_value.value;
 			var dien_tich_min = 0;
 			var dien_tich_max = window.RewayListValue.filter_max_value.value;
+			vm.center = [vm.ads.place.geo.lat,vm.ads.place.geo.lon];
+			vm.marker.content = vm.ads.giaFmt;
+			vm.marker.coords.latitude = vm.ads.place.geo.lat;
+			vm.marker.coords.longitude = vm.ads.place.geo.lon;
 
 		
 			var pageSize = 8;
@@ -32,6 +50,8 @@
 				vm.email="";
 				vm.content = "Tôi muốn tìm hiểu thêm thông tin về bất động sản tại " + window.location.href + ", xin vui lòng liên hệ lại sớm.";
 			}
+
+
 			if(vm.ads.place.diaChinh){
 				HouseService.findDuAnHotByDiaChinhForDetailPage({diaChinh: vm.ads.place.diaChinh}).then(function(res){
 					if(res.data.success==true)
@@ -103,6 +123,59 @@
         	vm.goChats = function(){
         		$state.go('mchats', { "adsID" : vm.ads.adsID}, {location: true});
         	}
+        	$timeout(function() {
+				$("#phgantaichinh").drawDoughnutChart([
+					{ title: "Gốc", value : 400,  color: "#20c063"},
+					{ title: "Lãi", value:  100,   color: "#f0a401"}
+			  	]);
+			}, 10);
+
+			var searchDataXungQuanh = {
+				"loaiTin": vm.ads.loaiTin,
+				"loaiNhaDat": 0, 
+				"limit": 9,
+				"soPhongNguGREATER": 0,
+	  			"soPhongTamGREATER": 0,
+	  			"soTangGREATER": 0,
+				//"diaChinh": vm.ads.place.diaChinh,
+				"place": {
+ 	      			//relandTypeName : window.RewayPlaceUtil.getTypeName(googlePlace),
+       				radiusInKm :  2,
+ 				    currentLocation: {
+ 				    	lat: vm.ads.place.geo.lat,
+ 				    	lon: vm.ads.place.geo.lon
+ 				    }
+				},
+				"updateLastSearch": false,
+			  	"orderBy": "ngayDangTinDESC",
+			  	"pageNo": 1
+			};
+
+			HouseService.findAdsSpatial(searchDataXungQuanh).then(function(res){
+				vm.nhaXungQuanh = res.data.list;					
+			});
+
+			var searchDataTuongTu = {
+				"loaiTin": vm.ads.loaiTin,
+				"loaiNhaDat": vm.ads.loaiNhaDat, 
+				"limit": 9,
+				"soPhongNguGREATER": 0,
+	  			"soPhongTamGREATER": 0,
+	  			"soTangGREATER": 0,
+				"diaChinh": {
+					tinh: vm.ads.place.diaChinh.tinhKhongDau,
+					huyen: vm.ads.place.diaChinh.huyenKhongDau
+				},
+				"updateLastSearch": false,
+			  	"orderBy": "ngayDangTinDESC",
+			  	"pageNo": 1
+			};
+
+			HouseService.findAdsSpatial(searchDataTuongTu).then(function(res){
+				vm.nhaTuongTu = res.data.list;					
+			});
+
+
 		});
 		
 	});
--- src/web/dist/app.bundle.js
+++ src/web/dist/app.bundle.js
@@ -65,7 +65,7 @@
 /******/ 	}
 /******/ 	
 /******/ 	var hotApplyOnUpdate = true;
-/******/ 	var hotCurrentHash = "a2bc1a2d14d7ecfdf61a"; // eslint-disable-line no-unused-vars
+/******/ 	var hotCurrentHash = "0fb14a486a8e54edd108"; // eslint-disable-line no-unused-vars
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentParents = []; // eslint-disable-line no-unused-vars
 /******/ 	
@@ -21933,10 +21933,24 @@
 		angular.module('bds').controller(controllerId,function ($rootScope, $http, $scope, $state, HouseService, NewsService, NgMap, $window,$timeout,$location){
 			var vm = this;
 			vm.adsID = $state.params.adsID;
+			vm.marker = {
+				id: 1,
+				coords: {
+					latitude: 	16.0439,
+					longitude: 	108.199
+				},
+				content: undefined,
+				data: 'test'
+			}
+			vm.center= [21.0363818591319,105.80105538518103];
 			HouseService.detailAds({adsID: vm.adsID, userID: $rootScope.user.userID}).then(function(res){
 				//console.log("res.data " + res.data.ads);
 				$rootScope.user.lastViewAds = vm.adsID;
 				vm.ads = res.data.ads;
+				vm.ads.chiTietThuGon = vm.ads.chiTiet;
+				if(vm.ads.chiTiet.length > 30){
+					vm.ads.chiTietThuGon = vm.ads.chiTiet.substring(0,300);				
+				}
 				vm.ads.place.diaChinh.tinhKhongDau =  window.RewayUtil.locDau(vm.ads.place.diaChinh.tinh);
 				vm.ads.place.diaChinh.huyenKhongDau =  window.RewayUtil.locDau(vm.ads.place.diaChinh.huyen);
 				vm.placeSearchText = vm.ads.place.diaChinh.huyen + "," + vm.ads.place.diaChinh.tinh;
@@ -21946,6 +21960,10 @@
 				var price_max = window.RewayListValue.filter_max_value.value;
 				var dien_tich_min = 0;
 				var dien_tich_max = window.RewayListValue.filter_max_value.value;
+				vm.center = [vm.ads.place.geo.lat,vm.ads.place.geo.lon];
+				vm.marker.content = vm.ads.giaFmt;
+				vm.marker.coords.latitude = vm.ads.place.geo.lat;
+				vm.marker.coords.longitude = vm.ads.place.geo.lon;
 
 			
 				var pageSize = 8;
@@ -21961,6 +21979,8 @@
 					vm.email="";
 					vm.content = "Tôi muốn tìm hiểu thêm thông tin về bất động sản tại " + window.location.href + ", xin vui lòng liên hệ lại sớm.";
 				}
+
+
 				if(vm.ads.place.diaChinh){
 					HouseService.findDuAnHotByDiaChinhForDetailPage({diaChinh: vm.ads.place.diaChinh}).then(function(res){
 						if(res.data.success==true)
@@ -22032,6 +22052,59 @@
 	        	vm.goChats = function(){
 	        		$state.go('mchats', { "adsID" : vm.ads.adsID}, {location: true});
 	        	}
+	        	$timeout(function() {
+					$("#phgantaichinh").drawDoughnutChart([
+						{ title: "Gốc", value : 400,  color: "#20c063"},
+						{ title: "Lãi", value:  100,   color: "#f0a401"}
+				  	]);
+				}, 10);
+
+				var searchDataXungQuanh = {
+					"loaiTin": vm.ads.loaiTin,
+					"loaiNhaDat": 0, 
+					"limit": 9,
+					"soPhongNguGREATER": 0,
+		  			"soPhongTamGREATER": 0,
+		  			"soTangGREATER": 0,
+					//"diaChinh": vm.ads.place.diaChinh,
+					"place": {
+	 	      			//relandTypeName : window.RewayPlaceUtil.getTypeName(googlePlace),
+	       				radiusInKm :  2,
+	 				    currentLocation: {
+	 				    	lat: vm.ads.place.geo.lat,
+	 				    	lon: vm.ads.place.geo.lon
+	 				    }
+					},
+					"updateLastSearch": false,
+				  	"orderBy": "ngayDangTinDESC",
+				  	"pageNo": 1
+				};
+
+				HouseService.findAdsSpatial(searchDataXungQuanh).then(function(res){
+					vm.nhaXungQuanh = res.data.list;					
+				});
+
+				var searchDataTuongTu = {
+					"loaiTin": vm.ads.loaiTin,
+					"loaiNhaDat": vm.ads.loaiNhaDat, 
+					"limit": 9,
+					"soPhongNguGREATER": 0,
+		  			"soPhongTamGREATER": 0,
+		  			"soTangGREATER": 0,
+					"diaChinh": {
+						tinh: vm.ads.place.diaChinh.tinhKhongDau,
+						huyen: vm.ads.place.diaChinh.huyenKhongDau
+					},
+					"updateLastSearch": false,
+				  	"orderBy": "ngayDangTinDESC",
+				  	"pageNo": 1
+				};
+
+				HouseService.findAdsSpatial(searchDataTuongTu).then(function(res){
+					vm.nhaTuongTu = res.data.list;					
+				});
+
+
 			});
 			
 		});
