5e24c18b4e47ab42e28d3107efc8c6247703fc20
hoailt <hoai0906@gmail.com>
2016-09-22 14:31:21 +0000

    find api - set default image

--- src/routes/api/findHandlerV2.js
+++ src/routes/api/findHandlerV2.js
@@ -44,7 +44,7 @@ function _handleDBFindResult(error, allAds, q) {
         cover: ads.image.cover ? ads.image.cover.replace("80x60", targetSize).replace("120x90", targetSize) : null,
         images: ads.image.images ? ads.image.images.map((e) => {
           return e.replace("80x60", targetSize);
-        }) : null
+        }) : [cfg.noCoverUrl]
       },
       diaChi: ads.place.diaChi,
       ngayDangTin: ads.ngayDangTin,
