d45eebb72241a1da3525c079d1491ee99c98325c
tinygipxy <hoai0906@gmail.com>
2016-10-11 15:44:26 +0800

    Cfg - Add db connectString

--- src/database/mydb.js
+++ src/database/mydb.js
@@ -1,12 +1,11 @@
-console.log("Call import mydb");
-
 var dbcfg = require("../config/db");
 
 var couchbase = require('couchbase');
 var cluster = new couchbase.Cluster(dbcfg.clusterUrl);
 var bucket = cluster.openBucket(dbcfg.dbName);
 bucket.enableN1ql([dbcfg.n1ql]);
-bucket.operationTimeout = 120 * 1000;
+bucket.operationTimeout = 240 * 1000;
 
+console.log("Call import mydb", dbcfg.clusterUrl);
 
 module.exports = bucket;
