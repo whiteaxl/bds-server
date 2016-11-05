b164743539e775cc48572cbf7676988edb0e2b28
tinygipxy <hoai0906@gmail.com>
2016-11-05 19:25:13 +0800

    Find Api - Fix bug detail image small for bds.com

--- src/routes/api/findHandlerV2.js
+++ src/routes/api/findHandlerV2.js
@@ -120,7 +120,7 @@ function _transform(allAds, q) {
       soPhongTam: ads.soPhongTam,
       soPhongTamFmt: ads.soPhongTam ? ads.soPhongTam + "p.tắm" : null,
       image: {
-        cover: ads.image.cover ? ads.image.cover.replace("80x60", targetSize).replace("120x90", targetSize) : cfg.noCoverUrl,
+        cover: ads.image.cover ? ads.image.cover.replace("80x60", targetSize).replace("120x90", targetSize).replace("200x200", targetSize) : cfg.noCoverUrl,
         images: ads.image.images ? ads.image.images.map((e) => {
           return e.replace("80x60", targetSize);
         }) : [cfg.noCoverUrl]
