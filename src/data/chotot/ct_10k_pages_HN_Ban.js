8e77537c781b8f4cad2f43ee16c2645d51ddd3ae
tinygipxy <hoai0906@gmail.com>
2016-12-02 01:09:04 +0800

    ChoTot - Try with 100pages first

--- src/data/chotot/ct_10k_pages_HN_Ban.js
+++ src/data/chotot/ct_10k_pages_HN_Ban.js
@@ -11,7 +11,7 @@ var bdsEx = new Extractor();
 DBCache.loadAdsRaw( " and source = 'chotot'", () => {
   console.log("Load done!");
 
-  bdsEx.extractFromTo("https://m.chotot.com/ha-noi/mua-ban-bat-dong-san", 100, 1000, () => {
+  bdsEx.extractFromTo("https://m.chotot.com/ha-noi/mua-ban-bat-dong-san", 100, 200, () => {
     console.log("Completed!");
     process.exit(0);
   });
