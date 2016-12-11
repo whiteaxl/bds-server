1bacf2363702aa144fecc45fe5aad5ee692ffd1e
tinygipxy <hoai0906@gmail.com>
2016-12-11 15:35:03 +0800

    DBCache - do searching concurrently

--- src/lib/DBCache.js
+++ src/lib/DBCache.js
@@ -28,6 +28,8 @@ global.rwcache = {};
 
 global.lastSyncTime = 0;
 
+var FIlTER_LIMIT = process.env.FIlTER_LIMIT || 100;
+
 
 function loadDoc(type, moreCondition, callback) {
   let sql = `select t.* from default t where type='${type}' `;
@@ -281,15 +283,65 @@ var cache = {
       }
     }
   },
+  _doSortingAndReturn(filtered, q, callback) {
+    let count = filtered.length;
+    console.log("Filterred length: ", count);
 
+    //sorting
+    let orderBy = q.orderBy || {"name": "ngayDangTin", "type":"DESC"};
+
+    let sign = 1;
+    if (orderBy.type == 'DESC') {
+      sign = -1;
+    }
+    console.log("Will sort by ", orderBy, sign);
+
+    let startTime = new Date().getTime();
+    filtered.sort((a, b) => {
+      if (a[orderBy.name] > b[orderBy.name]) {
+        return sign;
+      }
+
+      if (a[orderBy.name] < b[orderBy.name]) {
+        return -1 * sign;
+      }
+
+      if (a.timeExtracted > b.timeExtracted) {
+        return sign;
+      }
+      if (a.timeExtracted < b.timeExtracted) {
+        return -1 * sign;
+      }
+
+      if (a.id > b.id) {
+        return sign;
+      }
+
+      if (a.id < b.id) {
+        return -1 * sign;
+      }
+
+      return 0;
+    });
+    let endTime = new Date().getTime();
+
+    logUtil.info("Sorting time " + (endTime - startTime) + " ms for " + filtered.length + " records");
+
+    //do paging
+    filtered = filtered.slice((q.dbPageNo-1)*q.dbLimit, q.dbPageNo*q.dbLimit);
+
+
+
+    callback(null, filtered, count);
+    return filtered;
+  },
+  
   query(q, callback){
     let startQuery = new Date().getTime();
 
     if (q.huongNha && q.huongNha.length==1 && q.huongNha[0] == 0) {
       q.huongNha = null;
     }
-    //sorting
-    let orderBy = q.orderBy || {"name": "ngayDangTin", "type":"DESC"};
 
     let that = this;
 
@@ -303,61 +355,18 @@ var cache = {
       */
       .data();
 
-    async.filter(filteredByLoaiTin, (e, callback) => {
-      return that._match(q, e);
+    async.filterLimit(filteredByLoaiTin, FIlTER_LIMIT, (one, callbackFilter) => {
+      let truth = g_match(q, one);
+      //callbackFilter(null, truth);
+
+      // defer the callback
+      setImmediate(callbackFilter, null, truth);
     }, (err, filtered) => {
 
-      //ordering
-      let count = filtered.length;
-
-      console.log("Filterred length: ", count);
-
-      let sign = 1;
-      if (orderBy.type == 'DESC') {
-        sign = -1;
-      }
-      console.log("Will sort by ", orderBy, sign);
-
-      let startTime = new Date().getTime();
-      filtered.sort((a, b) => {
-        if (a[orderBy.name] > b[orderBy.name]) {
-          return sign;
-        }
-
-        if (a[orderBy.name] < b[orderBy.name]) {
-          return -1 * sign;
-        }
-
-        if (a.timeExtracted > b.timeExtracted) {
-          return sign;
-        }
-        if (a.timeExtracted < b.timeExtracted) {
-          return -1 * sign;
-        }
-
-        if (a.id > b.id) {
-          return sign;
-        }
-
-        if (a.id < b.id) {
-          return -1 * sign;
-        }
-
-        return 0;
-      });
-      let endTime = new Date().getTime();
-
-      logUtil.info("Sorting time " + (endTime - startTime) + " ms for " + filtered.length + " records");
-
-      //do paging
-      filtered = filtered.slice((q.dbPageNo-1)*q.dbLimit, q.dbPageNo*q.dbLimit);
+      that._doSortingAndReturn(filtered, q, callback);
 
       let endQuery = new Date().getTime();
-
       logUtil.info("Query time " + (endQuery - startQuery) + " ms for " + filtered.length + " records");
-
-      callback(null, filtered, count);
-      return filtered;
     });
   },
 
@@ -479,5 +488,124 @@ var cache = {
   }
 
 };
+
+
+function g_match(q, ads){
+  if (q.loaiTin !== ads.loaiTin) {
+    return false;
+  }
+
+  if(q.loaiNhaDat && _.indexOf(q.loaiNhaDat, ads.loaiNhaDat) === -1){
+    //logUtil.info("Not match loaiNhaDat", q.loaiNhaDat, ads.loaiNhaDat);
+    return false;
+  }
+
+  if (q.viewport) {
+    let vp = q.viewport;
+    let geo = ads.place.geo;
+
+    if (geo.lat < vp.southwest.lat || geo.lat > vp.northeast.lat
+      || geo.lon < vp.southwest.lon || geo.lon > vp.northeast.lon
+    ) {
+      //logUtil.info("Not match viewport", vp, geo);
+      return false;
+    }
+  }
+
+  if (q.diaChinh) {
+    let dc = q.diaChinh;
+    if (dc.tinhKhongDau && ads.place.diaChinh.codeTinh !== dc.tinhKhongDau) {
+      //logUtil.info("Not match codeTinh", dc.tinhKhongDau, ads.place.diaChinh.codeTinh);
+      return false;
+    }
+
+    if (dc.huyenKhongDau && ads.place.diaChinh.codeHuyen !== dc.huyenKhongDau) {
+      //logUtil.info("Not match codeHuyen", dc.huyenKhongDau, ads.place.diaChinh.codeHuyen);
+      return false;
+    }
+    if (dc.xaKhongDau && ads.place.diaChinh.codeXa !== dc.xaKhongDau) {
+      return false;
+    }
+    if (dc.duAnKhongDau && ads.place.diaChinh.codeDuAn !== dc.duAnKhongDau) {
+      return false;
+    }
+  }
+
+  if (q.ngayDangTinGREATER && ads.ngayDangTin <= q.ngayDangTinGREATER) { //ngayDangTinFrom: 20-04-2016
+    return false;
+  }
+
+  if (q.giaBETWEEN && (q.giaBETWEEN[0] > 0 || q.giaBETWEEN[1] < 9999999)) {
+    if (ads.gia < q.giaBETWEEN[0] || ads.gia > q.giaBETWEEN[1]) {
+      return false;
+    }
+  }
+
+  if(q.soPhongNguGREATER){
+    let soPhongNguGREATER = Number(q.soPhongNguGREATER);
+    if (soPhongNguGREATER && ads.soPhongNgu < soPhongNguGREATER) {
+      return false;
+    }
+  }
+
+
+  if(q.soPhongTamGREATER){
+    let soPhongTamGREATER = Number(q.soPhongTamGREATER);
+    if (soPhongTamGREATER && ads.soPhongTam < soPhongTamGREATER) {
+      return false;
+    }
+  }
+  if(q.soTangGREATER){
+    let soTangGREATER = Number(q.soTangGREATER);
+    if (soTangGREATER && ads.soTang < soTangGREATER) {
+      return false;
+    }
+  }
+
+  if ((q.dienTichBETWEEN) && (q.dienTichBETWEEN[0] > 0 || q.dienTichBETWEEN[1] < 9999999)) {
+    if (ads.dienTich < q.dienTichBETWEEN[0] || ads.dienTich > q.dienTichBETWEEN[1]) {
+      return false;
+    }
+  }
+
+  if(q.huongNha  && _.indexOf(q.huongNha, ads.huongNha) === -1){
+    return false;
+  }
+
+  if(q.soPhongNgu){
+    let soPhongNgu = Number(q.soPhongNgu);
+    if (ads.soPhongNgu !== soPhongNgu) {
+      return false;
+    }
+  }
+  if(q.soPhongTam){
+    let soPhongTam = Number(q.soPhongTam);
+    if (ads.soPhongTam !== soPhongTam) {
+      return false;
+    }
+  }
+
+  if(q.soTang){
+    let soTang = Number(q.soTang);
+    if (ads.soTang !== soTang) {
+      return false;
+    }
+  }
+
+  if (q.gia) {
+    if (ads.gia !== q.gia) {
+      return false;
+    }
+  }
+
+  if (q.dienTich) {
+    if (ads.dienTich !== q.dienTich) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 
 module.exports = cache;
